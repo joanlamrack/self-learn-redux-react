848caca778418579c25b1ba5f24b690cb846f3f3
Joan Lamrack <joanlamrack@gmail.com>
2018-09-05 08:04:11 +0700

    back

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
-import index from "./js/index";
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
